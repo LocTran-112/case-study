c6ee01bd197c702e1e02a01f6e712a7062c47780
Lộc Trần <loctran@Locs-MacBook-Air.local>
2023-07-05 20:54:20 +0700

    final update

--- src/Components/Reuse/Header.js
+++ src/Components/Reuse/Header.js
@@ -1,29 +1,29 @@
 // import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import withModal from '../Reuse/Modal';
-// import Create from '../create/Create';
-// import Context from '../../Context';
+import Create from '../Create/Create';
+//  import Context from '../../Context';
 import logo from '../../logo.svg';
 
 const Header = (props) => {
-    // const { toggleModal } = props;
+    const { toggleModal } = props;
 
-    // const { user } = useContext(Context);
+    //  const { user } = useContext(Context);
 
     const history = useNavigate();
 
-    const logout = async () => {
+    function logout(){
         const isLogout = window.confirm('Do you want to log out ?');
         if (isLogout) {
-            await history.push('/login');
+           history('/login');
         }
     };
 
-    // const goCreatePost = () => {
-    //     toggleModal(true);
-    // };
+    const goCreatePost = () => {
+        toggleModal(true);
+    };
 
-    const goHome = () => {
+    function goHome(){
         history('/');
     };
 
@@ -48,9 +48,9 @@ const Header = (props) => {
                 <span onClick={goHome}>
                     <svg aria-label="Home" className="_8-yf5 " color="#262626" fill="#262626" height="22" role="img" viewBox="0 0 48 48" width="22"><path d="M45.3 48H30c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2-4.6-4.6-4.6s-4.6 2-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.5-.6 2.1 0l21.5 21.5c.4.4.6 1.1.3 1.6 0 .1-.1.1-.1.2v22.8c.1.8-.6 1.5-1.4 1.5zm-13.8-3h12.3V23.4L24 3.6l-20 20V45h12.3V34.2c0-4.3 3.3-7.6 7.6-7.6s7.6 3.3 7.6 7.6V45z"></path></svg>
                 </span>
-                {/* <span onClick={goCreatePost}>
+                <span onClick={goCreatePost}>
                     <svg aria-label="New Post" className="_8-yf5 " color="#262626" fill="#262626" height="22" role="img" viewBox="0 0 48 48" width="22"><path d="M31.8 48H16.2c-6.6 0-9.6-1.6-12.1-4C1.6 41.4 0 38.4 0 31.8V16.2C0 9.6 1.6 6.6 4 4.1 6.6 1.6 9.6 0 16.2 0h15.6c6.6 0 9.6 1.6 12.1 4C46.4 6.6 48 9.6 48 16.2v15.6c0 6.6-1.6 9.6-4 12.1-2.6 2.5-5.6 4.1-12.2 4.1zM16.2 3C10 3 7.8 4.6 6.1 6.2 4.6 7.8 3 10 3 16.2v15.6c0 6.2 1.6 8.4 3.2 10.1 1.6 1.6 3.8 3.1 10 3.1h15.6c6.2 0 8.4-1.6 10.1-3.2 1.6-1.6 3.1-3.8 3.1-10V16.2c0-6.2-1.6-8.4-3.2-10.1C40.2 4.6 38 3 31.8 3H16.2z"></path><path d="M36.3 25.5H11.7c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5h24.6c.8 0 1.5.7 1.5 1.5s-.7 1.5-1.5 1.5z"></path><path d="M24 37.8c-.8 0-1.5-.7-1.5-1.5V11.7c0-.8.7-1.5 1.5-1.5s1.5.7 1.5 1.5v24.6c0 .8-.7 1.5-1.5 1.5z"></path></svg>
-                </span> */}
+                </span>
                 {/* <span onClick={goChat}>
                     <svg xmlns="http://www.w3.org/2000/svg" style={{ width: '1.75rem', height: '1.75rem' }} className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
@@ -70,4 +70,4 @@ const Header = (props) => {
     );
 }
 
-export default withModal(Header);
+export default withModal(Create)(Header);
